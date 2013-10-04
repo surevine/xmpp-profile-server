e045c224682a9bef3121f8bc7fbc5de35ddea1a0
Lloyd Watkin <lloyd.watkin@surevine.com>
2013-10-04 13:26:36 +0100

    Add cascade delete

--- database/0001.sql
+++ database/0001.sql
@@ -10,7 +10,7 @@ CREATE TABLE owners ("owner" TEXT NOT NULL,
     last_updated TIMESTAMP,
     PRIMARY KEY ("owner"));
     
-CREATE TABLE vcards ("owner" TEXT NOT NULL REFERENCES owners("owner"),
+CREATE TABLE vcards ("owner" TEXT NOT NULL REFERENCES owners("owner") ON DELETE CASCADE,
     vcard TEXT,
     name TEXT NOT NULL,
     "default" BOOLEAN DEFAULT false,
@@ -18,12 +18,12 @@ CREATE TABLE vcards ("owner" TEXT NOT NULL REFERENCES owners("owner"),
     last_updated TIMESTAMP,
     PRIMARY KEY ("owner", name));
 
-CREATE TABLE roster (owner TEXT NOT NULL REFERENCES owners("owner"),
+CREATE TABLE roster (owner TEXT NOT NULL REFERENCES owners("owner") ON DELETE CASCADE,
     "group" TEXT NOT NULL,
     "user" TEXT NOT NULL,
     PRIMARY KEY ("owner", "user"));
 
-CREATE TABLE rostermap (owner TEXT NOT NULL REFERENCES owners("owner"),
+CREATE TABLE rostermap (owner TEXT NOT NULL REFERENCES owners("owner") ON DELETE CASCADE,
     "group" TEXT NOT NULL,
     vcard TEXT NOT NULL,
     PRIMARY KEY ("owner", "group"));
