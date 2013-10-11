f490a2e0a82ae2cacf8e9543c1507b74bb8e5a2f
Lloyd Watkin <lloyd.watkin@surevine.com>
2013-10-11 15:44:22 +0100

    Adding VCardTest file

---
+++ src/test/java/com/surevine/profileserver/db/jdbc/JDBCDataStoreTest/VCardTest.java
@@ -0,0 +1,74 @@
+package com.surevine.profileserver.db.jdbc.JDBCDataStoreTest;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.xmpp.packet.JID;
+
+import com.surevine.profileserver.db.jdbc.DatabaseTester;
+import com.surevine.profileserver.db.jdbc.JDBCDataStore;
+import com.surevine.profileserver.db.jdbc.dialect.Sql92DataStoreDialect;
+import com.surevine.profileserver.helpers.IQTestHandler;
+
+public class VCardTest {
+
+	DatabaseTester dbTester;
+	Connection conn;
+
+	JDBCDataStore store;
+	private JID ownerJid = new JID("owner@example.com");
+	private JID userJid = new JID("user@server.co.uk/desktop");
+	private JID noPublicJid = new JID("nopublic@server.com");
+	private String group = "friends";
+
+	public VCardTest() throws SQLException, IOException,
+			ClassNotFoundException {
+		dbTester = new DatabaseTester();
+		IQTestHandler.readConf();
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		dbTester.initialise();
+
+		store = new JDBCDataStore(dbTester.getConnection(),
+				new Sql92DataStoreDialect());
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		dbTester.close();
+	}
+
+	@Test
+	public void testCanAddRosterItem() throws Exception {
+		dbTester.loadData("basic-data");
+		Assert.assertEquals("friends-false-4", store.getVcardForUser(ownerJid, userJid));
+	}
+	
+	@Test
+	public void testCanGetPublicVCard() throws Exception {
+		dbTester.loadData("basic-data");
+		Assert.assertEquals("family-true-2", store.getPublicVcard(ownerJid));
+	}
+	
+	@Test
+	public void testGetNullWhenNoPublicVcard() throws Exception {
+		dbTester.loadData("basic-data");
+		store.addOwner(noPublicJid);
+		Assert.assertNotNull(store.getPublicVcard(noPublicJid));
+	}
+	
+	@Test
+	public void testGetNullWhenNoAppropriateVCardForUser() throws Exception {
+		dbTester.loadData("basic-data");
+		Assert.assertNull(store.getVcardForUser(ownerJid, ownerJid));
+	}
+}
